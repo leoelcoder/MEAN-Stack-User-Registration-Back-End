67b8a3581baa73553cd53cd186a70fff48bc135e
Leonardo Lopez <leonard_156@hotmail.com>
2025-09-27 00:51:51 -0600

    Dockerized app and Deploy workflow

--- Project/models/db.js
+++ Project/models/db.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, (err) => {
+const {
+    MONGO_INITDB_ROOT_USERNAME,
+    MONGO_INITDB_ROOT_PASSWORD,
+    MONGO_INITDB_URL,
+    MONGO_INITDB_PORT
+} = process.env;
+
+const uri = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${encodeURIComponent(MONGO_INITDB_ROOT_PASSWORD)}@${MONGO_INITDB_URL}:${MONGO_INITDB_PORT}/admin`;
+
+
+mongoose.connect(uri, (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
